ad691d2742280c896712c96b22ca42a0069c3fd5
Offshoots <252246959+Offshoots@users.noreply.github.com>
2026-01-06 21:28:35 -0800

    1_6_2026_u2
    Functional Tool Animations and swapping.
    1:00:53 timestamp of tutorial video.

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -4,7 +4,8 @@ var direction: Vector2
 var speed = 50
 
 @onready var move_state_machine = $Animation/AnimationTree.get("parameters/MoveStateMachine/playback")
-
+@onready var tool_state_machine = $Animation/AnimationTree.get("parameters/ToolStateMachine/playback")
+var current_tool: Enum.Tool = Enum.Tool.SWORD
 
 func _physics_process(_delta: float) -> void:
 	get_basic_input()
@@ -12,7 +13,13 @@ func _physics_process(_delta: float) -> void:
 	animate()
 
 func get_basic_input():
+	if Input.is_action_just_pressed("tool_forward") or Input.is_action_just_pressed("tool_backward"):
+		var dir = Input.get_axis("tool_backward", "tool_forward")
+		current_tool = posmod((current_tool + int(dir)), Enum.Tool.size()) as Enum.Tool
+		print(current_tool)
+		
 	if Input.is_action_just_pressed("action"):
+		tool_state_machine.travel(Data.TOOL_STATE_ANIMATIONS[current_tool])
 		$Animation/AnimationTree.set("parameters/ToolOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 		
 
